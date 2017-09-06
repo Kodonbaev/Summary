61187b1f4fd7a56d29b77d874a65515f43509132
Aktilek <aktilek.sof@gmail.com>
2017-09-06 14:47:45 +0600

    push favicon

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $('.skill-text').each(function () {
   $(this).prop('Counter',0).animate({
     Counter:$(this).text()
   },{
-    duration: 4000,
+    duration: 8000,
     easing: 'swing',
     step:function(now){
       $(this).text(Math.ceil(now));
